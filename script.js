09967a6b2d3dadba71b00517310dbd43ce30a18c
Ashley Oliver <jeangray218@gmail.com>
2023-03-23 23:15:59 -0400

    first commit

--- script.js
+++ script.js
@@ -7,41 +7,73 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuote = function () {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
+
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
-
+  let mainTitle = document.querySelector("#main-title")
+  mainTitle.textContent = "Welcome to the Dom"
   // Part 2
-
+  let bodyYody = document.querySelector('body')
+  bodyYody.style.backgroundColor = "lavender"
 
   // Part 3
+  let domFavThings = document.querySelector("#favorite-things")
 
-
+  domFavThings.removeChild(domFavThings.children[5])
   // Part 4
-
+  const specialTitles = document.querySelectorAll('.special-title')
+  specialTitles.forEach(function (title) {
+    title.style.fontSize = "2rem"
+  })
 
   // Part 5
 
+  const pastRaces = document.querySelector('#past-races')
+  pastRaces.removeChild(pastRaces.children[3])
+
 
   // Part 6
-
+  let newRace = document.createElement('li')
+  newRace.textContent = "Atlanta"
+  pastRaces.appendChild(newRace)
 
   // Part 7
+  let main = document.querySelector('.main')
+  let blogPost = document.createElement('div')
+  blogPost.classList.add('blog-post', 'purple')
+  main.appendChild(blogPost)
 
+
+  let blogTitle = document.createElement('h1')
+  blogTitle.textContent = 'Atlanta'
+  blogPost.appendChild(blogTitle)
+
+  let newPTag = document.createElement('p')
+  newPTag.textContent = "I raced down Spring St...the wrong way!"
+  blogPost.appendChild(newPTag)
 
   // Part 8
-
-
+  let randoQuote = document.querySelector('#quote-title')
+  randoQuote.addEventListener('click', function (event) {
+    randomQuote();
+  })
   // Part 9
-
+  let blogs = document.querySelectorAll('.blog-post');
+  blogs.forEach(element => {
+    element.addEventListener('mouseout', function (event) {
+      element.classList.toggle('purple')
+    });
+    element.addEventListener('mouseenter', function (event) {
+      element.classList.toggle('red');
+    });
+  });
 
 
 
